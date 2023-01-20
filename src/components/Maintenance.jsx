d26328f8230b08dd485b5bb197a9fec53a00747a
ljstaib <ljstaib@bu.edu>
2023-01-20 18:35:39 -0500

    Further changes to maintenance

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './styles.module.css'
 
 export default function SiteHeader() {
 
     return (
         <html>
-            <h1>404</h1>
-            <p>Site is currently down for maintenance. Check back soon!</p>
+            <h1 className={styles.maintenanceHead}>404</h1>
+            <p className={styles.maintenanceBody}>Site is currently down for maintenance. Check back soon!</p>
         </html>
     )
 };
