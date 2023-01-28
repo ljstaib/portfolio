3cde2f0bf5a3f88992e953a1ce49a1b0ec189c52
ljstaib <ljstaib@bu.edu>
2023-01-28 01:02:06 -0500

    Fixed a bug.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,8 @@ import SiteFooter from './components/SiteFooter';
 
 import Maintenance from './components/Maintenance';
 
-import ReactGA from 'react-ga4';
-
 function App() {
 	const doMaintenance = true;
-	ReactGA.pageview(window.location.pathname + window.location.search);
 
 	if (doMaintenance) {
 		return (
