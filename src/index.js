d9a736e01cf7c225fe5e713461c1c33a504b926f
ljstaib <ljstaib@bu.edu>
2023-01-28 20:02:58 -0500

    New method for google analytics

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import ReactGA from 'react-ga4';
-ReactGA.initialize(process.env.REACT_APP_T_ID);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
