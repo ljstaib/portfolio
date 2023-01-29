e84005ffd99d7cce6979ef52fe02fef6589b2e58
ljstaib <ljstaib@bu.edu>
2023-01-28 23:08:52 -0500

    Fixed room clutter description

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ import RoomClutter from '../assets/projects/RoomClutter.png'
 import C2 from '../assets/projects/C2.png'
 
 export default function Projects() {
-	const rc_description = "Worked on a team of four to create a framework for a React Native application that can take a picture of a living space and determine how cluttered that living space is. Worked on user authentication, file upload and storage, in-app display of saved rooms, online versus offline capabilities, and an administrative web application. The application used Google Firebase for its database."
+	const rc_description = "Worked on a team of four to create a framework for a React Native application that can take a picture of a living space and determine how cluttered it is. Worked on user authentication, file upload and storage, display of saved rooms, online versus offline capabilities, and an administrative web application. The application used Google Firebase for its database."
 	const c2_description = "C2 server created in Python (Flask) as a command hub to infect computers and run tasks through malicious implants. Implant capabilities (execution, encyrption, looting, file I/O, situational awareness, persistence, and HTTP/S communication) were created in C++ using the Windows API. "
 
 	return (
