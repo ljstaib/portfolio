ac84d875f9667a7d7cbe6ba9374d9c38df6f8d9d
ljstaib <ljstaib@bu.edu>
2023-02-10 13:41:47 -0500

    Fixed minor spelling error

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ import C2 from '../assets/projects/C2.png'
 
 export default function Projects() {
 	const rc_description = "Worked on a team of four to create a framework for a React Native application that can take a picture of a living space and determine how cluttered it is. Worked on user authentication, file upload and storage, display of saved rooms, online versus offline capabilities, and an administrative web application. The application used Google Firebase for its database."
-	const c2_description = "C2 server created in Python (Flask) as a command hub to infect computers and run tasks through malicious implants. Implant capabilities (execution, encyrption, looting, file I/O, situational awareness, persistence, and HTTP/S communication) were created in C++ using the Windows API. "
+	const c2_description = "C2 server created in Python (Flask) as a command hub to infect computers and run tasks through malicious implants. Implant capabilities (execution, encryption, looting, file I/O, situational awareness, persistence, and HTTP/S communication) were created in C++ using the Windows API. "
 
 	return (
 		<div>
